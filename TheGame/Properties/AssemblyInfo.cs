bedf02b150a5293f1c1332b93366911c0cf9de7e
Philippe Hürlimann <p@hurlimann.org>
2016-12-26 22:37:36 +0100

    Converted to MonoGame/MonoDevelop

---
+++ TheGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+#if __ANDROID__
+using Android.App;
+#endif
+
+[assembly: AssemblyTitle("TheGame")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct("TheGame")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.0")]
